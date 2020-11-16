8cb88489fba1f3bf6b97a7940b72d6facd30fa5f
Mike Baldwin <mikebaldwin21@gmail.com>
2020-11-15 19:49:05 -0500

    -Added copyright notice file headers

--- Texture.h
+++ Texture.h
@@ -1,4 +1,12 @@
-  
+/*
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ *
+ * Copyright 2020 by Michael Baldwin <michael.baldwin3@rockets.utoledo.edu>
+ *				 and Ethan Hassett <ethan.hassett@rockets.utoledo.edu>
+ */
+
+
 #pragma once
 
 #include "Renderer.h"
--- Time.cpp
+++ Time.cpp
@@ -1,3 +1,11 @@
+/*
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ *
+ * Copyright 2020 by Michael Baldwin <michael.baldwin3@rockets.utoledo.edu>
+ *				 and Ethan Hassett <ethan.hassett@rockets.utoledo.edu>
+ */
+
 #include "Time.h"
 #include <SDL.h>
 
